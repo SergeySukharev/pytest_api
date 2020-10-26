b4f9cdf40ce40965865b6da05e5851553d38c29d
Sergey Sukhartev <qwerty255000@gmail.com>
2020-10-26 10:47:45 +0300

    Refactoring

--- test_api.py
+++ test_api.py
@@ -19,14 +19,13 @@ def test_get_negative(session, base_url, post_id):
     assert res.json() == {}
 
 
-@pytest.mark.parametrize('keys', [['userId', 'id', 'title', 'completed']])
-def test_get_all(session, base_url, keys):
+def test_get_all(session, base_url):
     res = session.get(url=f'{base_url}')
 
     assert len(res.json()) == POSTS_MAX
     for elem in res.json():
         for key in elem.keys():
-            assert key in keys
+            assert key in ['userId', 'id', 'title', 'completed']
 
 
 def test_post(session, base_url):
@@ -44,31 +43,36 @@ def test_post(session, base_url):
 
 
 def test_put_positive(session, base_url):
-    post_id = 1
-    title = 'foo'
-    body = 'bar'
-    payload = {'title': title, 'body': body, 'id': post_id, 'userId': 1}
-    res = session.put(url=f'{base_url}/{post_id}', json=payload)
+    payload = {'title': 'foo', 'body': 'bar', 'id': 1, 'userId': 1, 'completed': True}
+    res = session.put(url=f'{base_url}/{payload["id"]}', json=payload)
 
     assert res.status_code == 200
     res_json = res.json()
-    assert res_json['title'] == title
-    assert res_json['body'] == body
+    assert res_json['title'] == payload['title']
+    assert res_json['body'] == payload['body']
+    assert res_json['id'] == payload['id']
+    assert res_json['completed'] == payload['completed']
 
 
-def test_put_negative(session, base_url):
-    payload = 11232
-    res = session.put(url=f'{base_url}/1', json=payload)
+@pytest.mark.parametrize('post_id, data',
+                         [(1, -1), (201, {'title': 'foo', 'body': 'bar', 'id': 1, 'userId': 1, 'completed': True})])
+def test_put_negative(session, base_url, post_id, data):
+    res = session.put(url=f'{base_url}/{post_id}', json=data)
 
     assert res.status_code == 500
 
 
-@pytest.mark.parametrize('data', [{"userId": '88'}, {"id": '66'}, {"title": "test title"}, {"completed": 'True'}])
-def test_patch(session, base_url, data):
-    res = session.patch(url=f'{base_url}/1', data=data)
+@pytest.mark.parametrize('field, value', [
+    ("userId", 88),
+    ("id", 66),
+    ("title", "test title"),
+    ("completed", True)
+])
+def test_patch(session, base_url, field, value):
+    res = session.patch(url=f'{base_url}/1', json={field: value})
 
     assert res.status_code == 200
-    assert res.json()[list(data)[0]] == data[list(data)[0]]
+    assert res.json()[field] == value
 
 
 def test_delete(session, base_url):
@@ -78,13 +82,22 @@ def test_delete(session, base_url):
     assert not res.json()
 
 
-@pytest.mark.parametrize('params', [{"userId": 9}, {"id": 4}, {"title": "vel non beatae est"}, {"completed": True}])
-def test_filter_positive(session, base_url, params):
-    res = session.get(url=f'{base_url}', params=params)
+@pytest.mark.parametrize('field, value', [
+    ("userId", 9),
+    ("id", 4),
+    ("title", 'vel non beatae est'),
+    ("completed", False),
+    ("completed", True)
+])
+def test_filter_positive(session, base_url, field, value):
+    str_val = str(value).lower() if isinstance(value, bool) else str(value)
+    res = session.get(url=f'{base_url}', params={field: str_val})
 
     assert res.status_code == 200
-    for elem in res.json():
-        assert elem[list(params)[0]] == params[list(params)[0]]
+    r_json = res.json()
+    assert len(r_json) > 0
+    for elem in r_json:
+        assert elem[field] == value
 
 
 @pytest.mark.parametrize('params', [{"userId": 'User'}, {"id": 5676}, {"title": "there is no such title"},
